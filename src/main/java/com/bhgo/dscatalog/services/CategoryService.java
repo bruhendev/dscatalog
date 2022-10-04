9d1ebe903f10d0fc217acf3b7de9e14639d55d57
Bruno Henrique <brunohenrique242@gmail.com>
2022-10-04 14:07:43 -0300

    DTO

--- src/main/java/com/bhgo/dscatalog/services/CategoryService.java
+++ src/main/java/com/bhgo/dscatalog/services/CategoryService.java
@@ -1,21 +1,26 @@
 package com.bhgo.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bhgo.dscatalog.dto.CategoryDTO;
 import com.bhgo.dscatalog.entities.Category;
 import com.bhgo.dscatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
 
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
 	}
-	
+
 }
