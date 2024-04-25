839e0494e64be16516a75e7a6ea214cc0de7837d
DESKTOP-C22J861\Bruno <brunohenrique242@gmail.com>
2024-04-25 14:06:54 -0300

    refactor: CategoryResource and CategoryService update findAllPaged with param Pageable

--- backend/src/main/java/com/bruno/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/bruno/dscatalog/services/CategoryService.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,16 +17,15 @@ import com.bruno.dscatalog.services.exceptions.ResourceNotFoundException;
 
 import jakarta.persistence.EntityNotFoundException;
 
-
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
-		Page<Category> list = repository.findAll(pageRequest);
+	public Page<CategoryDTO> findAllPaged(Pageable pageable) {
+		Page<Category> list = repository.findAll(pageable);
 		return list.map(x -> new CategoryDTO(x));
 	}
 
@@ -41,7 +40,7 @@ public class CategoryService {
 	public CategoryDTO insert(CategoryDTO dto) {
 		Category entity = new Category();
 		entity.setName(dto.getName());
-		entity =  repository.save(entity);
+		entity = repository.save(entity);
 		return new CategoryDTO(entity);
 	}
 
@@ -61,13 +60,12 @@ public class CategoryService {
 		if (!repository.existsById(id)) {
 			throw new ResourceNotFoundException("Recurso não encontrado");
 		}
-		
+
 		try {
 			repository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException("Integrity violation");
 		}
-		
-		
+
 	}
 }
